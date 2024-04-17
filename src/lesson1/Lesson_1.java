0f58c37523c0d09541f620c56a275a866c62a7f6
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-04-17 21:00:58 +0300

    Add class Lesson_1

---
+++ src/lesson1/Lesson_1.java
@@ -0,0 +1,10 @@
+package src.lesson1;
+
+public class Lesson_1 {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+}
