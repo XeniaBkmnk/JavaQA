9ad11360fed8426cea7473b406b9a902a8224773
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-04-18 01:03:31 +0300

    add method replaceValue()

--- src/lesson1/Lesson_1.java
+++ src/lesson1/Lesson_1.java
@@ -74,4 +74,16 @@ public class Lesson_1 {
         System.out.println(isLeapYear);
     }
 
+    public static void replaceValue() {
+        int[] array = {1, 1, 1, 0, 0, 1, 1, 0};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                array[i] = 1;
+            } else if (array[i] == 1) {
+                array[i] = 0;
+            }
+            System.out.print(array[i]);
+        }
+    }
+
 }
