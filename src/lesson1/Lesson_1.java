e65ffe0e4600b8b1a6f6246968dd9a858c548677
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-04-17 21:32:07 +0300

    add method checkSumSign()

--- src/lesson1/Lesson_1.java
+++ src/lesson1/Lesson_1.java
@@ -7,4 +7,15 @@ public class Lesson_1 {
         System.out.println("Apple");
     }
 
+    public static void checkSumSign() {
+        int a = 22;
+        int b = -1;
+        int x = a + b;
+
+        if (x>=0){
+            System.out.println("Сумма положительная");
+        }else
+            System.out.println("Сумма отрицательная");
+    }
+
 }
