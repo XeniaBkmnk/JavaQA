9f32cf9ef3e82b34b273c3439909ba67c3663713
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-04-18 00:25:47 +0300

    refact method booleanLeapYear()

--- src/lesson1/Lesson_1.java
+++ src/lesson1/Lesson_1.java
@@ -70,10 +70,8 @@ public class Lesson_1 {
 
     public static void booleanLeapYear() {
         int year = 2024;
-        System.out.println(
-                year % 400 == 0 ? "True" :
-                        year % 4 == 0 && year % 100 != 0 ? "true" : "False"
-        );
+        boolean isLeapYear = year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+        System.out.println(isLeapYear);
     }
 
 }
